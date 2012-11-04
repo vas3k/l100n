ba5adae5fa390b67afbf2b7859cf88bfd72194a0
vas3k <vas3k007@gmail.com>
2012-11-04 16:37:11 +0700

    Version 1.0

---
+++ l100n.js
@@ -0,0 +1,146 @@
+/*
+ * l100n 1.0 - unusual JavaScript library for pure-client localization
+ *
+ * Full description in Russian: http://vas3k.ru/dev/l100n
+ *
+ * Licensed under the LGPL licenses:
+ *   http://www.gnu.org/licenses/lgpl.html
+ *
+ */
+
+l100n = {
+	locale: "ru",                   // Current locale codename
+	default_locale: "ru",           // Default (fallback) locale codename
+	pages: {
+        "test-page": {              // Page codename
+            "test-string": {        // String codename
+                selector: "#test",  // CSS-selector for replace strings
+                en: "Testing",      // Locales
+                ru: "Тестинг"
+            }
+        }
+	},
+
+    /**
+     * Add page localizationn dictionary to localization object
+     * @param page_name (required) - page codename
+     * @param page_strings (optional) - l10n dictionary
+     */
+    add_page: function(page_name, page_strings) {
+        if (page_name) {
+            this.pages[page_name] = page_strings;            
+            return true;
+        }
+        return false;
+    },
+
+    /**
+     * Localize element by codename
+     * @param name (required) - string codename
+     * @param custom_page (optional) - page codename, if not provided causes full scan for string codename
+     * @param custom_locale (optional) - custom locale codename
+     */
+    localize: function(name, custom_page, custom_locale) {
+        var loc = custom_locale || this.locale || this.default_locale;
+
+        var l10n_object = this.get_l10n_object(name, custom_page);
+        if (!l10n_object) return false;
+
+        // Try to get localization
+        var l10n_string = l10n_object[loc];
+        if (!l10n_string) {
+            // In case of failure, try to get default localization
+            l10n_string = l10n_object[this.default_locale];
+            // If not default localization - fail
+            if (!l10n_string) return false;
+        }
+
+        // Localizing element
+        $(l10n_object.selector).html(l10n_string);
+        return true;
+    },
+
+    /**
+     * Returns localized string for codename
+     * @param name (required) - string codename
+     * @param custom_page (optional) - page codename, if not provided causes full scan for string codename
+     * @param custom_locale (optional) - custom locale codename
+     */
+    localize_string: function(name, custom_page, custom_locale) {
+        var loc = custom_locale || this.locale || this.default_locale;
+
+        var l10n_object = this.get_l10n_object(name, custom_page);
+        if (!l10n_object) return "";
+
+        // Try to get localization
+        var l10n_string = l10n_object[loc];
+        if (!l10n_string) {
+            // In case of failure, try to get default localization
+            l10n_string = l10n_object[this.default_locale];
+            // If not default localization - fail
+            if (!l10n_string) return "";
+        }
+
+        // Returns localized string
+        return l10n_string;
+    },
+
+    /**
+     * Localize entire page by page codename
+     * @param page (required) - page codename
+     * @param custom_locale (optional) - custom locale codename
+     */
+    localize_page: function(page, custom_locale) {
+        if (!page) return false;
+        var loc = custom_locale || this.locale || this.default_locale;
+
+        var strings = this.pages[page];
+        for (var string_id in strings) {
+            var string = strings[string_id];
+            $(string.selector).html(string[loc]);
+        }
+
+        return true;
+    },
+
+    /**
+     * Localize entire all pages (may be slow)
+     * @param custom_locale (optional) - custom locale codename
+     */
+    localize_all_pages: function(custom_locale) {
+        var loc = custom_locale || this.locale || this.default_locale;
+
+        for (var page_name in this.pages) {
+            this.localize_page(page_name, loc);
+        }
+
+        return true;
+    },
+
+    /**
+     * (private method) Returns localization object by string codename
+     * @param name (required) - string codename
+     * @param custom_page (optional) - page codename, if not provided causes full scan for string codename
+     */
+    get_l10n_object: function(name, custom_page) {
+        // If custom_page not provided start full search
+        // until first occurrence of string codename
+        var page = custom_page;
+        if (!page) {
+            var found = false;
+            for (var page_name in this.pages) {
+                var strings = this.pages[page_name];
+                for (var string_id in strings) {
+                    if (string_id == name) {
+                        page = page_name;
+                        found = true;
+                        break;
+                    }
+                }
+                if (found) break;
+            }
+        }
+
+        return this.pages[page][name];
+    }
+};
